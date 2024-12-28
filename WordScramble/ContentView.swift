c7c38aaacb13cffe9e21d8a7dd3d27b39a145138
kevindarmawan <kevin.darmawan@shopee.com>
2024-12-28 21:36:05 +0700

    Project 5 part 2 day 30

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,66 +8,137 @@
 import SwiftUI
 
 struct ContentView: View {
-
-    let people = ["Finn", "Leia", "Luke", "Rey"]
-
-    var body: some View {
-        VStack {
-            List {
-                Section {
-                    Text("Static row 1")
-                    Text("Static row 2")
-                }
-                Section {
-                    ForEach(0..<5) {
-                        Text("Dynamic row \($0)")
-                    }
-                }
-                Section {
-                    Text("Static row 3")
-                    Text("Static row 4")
-                }
-            }
-            List(people, id: \.self) {
-                Text($0)
-            }
-            List {
-                Text("Static Row")
-                ForEach(people, id: \.self) {
-                    Text($0)
-                }
-                Text("Static Row")
-            }
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = "Gaming"
+    @State private var newWord = ""
+    // error control var
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    func addNewWord() {
+        // lowercase and trim whitespace newline
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        // check atleast 1 character
+        guard answer.count > 0 else { return }
+        // check originality
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Think of a new word")
+            return
         }
-        VStack {
-            if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-                // we found the file in our bundle!
-                if let fileContents = try? String(contentsOf: fileURL, encoding: .ascii) {
-                    // we loaded the file into a string!
-                }
-            }
+        // check word validity
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
         }
-    }
-    func testString() -> [String]{
-        let input = "a b c"
-        let input2 = """
-                     a
-                     b
-                     c
-                     """
-        let letters = input.components(separatedBy: " ")
-        let _ = input2.components(separatedBy: "\n")
-        let _ = letters.randomElement()
-        let _ = input.trimmingCharacters(in: .whitespacesAndNewlines)
-        return letters
+        // check real word
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't use imaginary words")
+            return
+        }
+        
+        // insert at position 0
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        print(usedWords)
+        // reset newWord
+        newWord = ""
     }
     
-    func testWord() {
-        let word = "swift"
+    func startGame() {
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL, encoding: .ascii) {
+                // 3. Split the string up into an array of strings, split by line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+                // 4. Pick one random word, or use "creator" as a default
+                rootWord = allWords.randomElement() ?? "silkworm"
+                // If we are here then everything has worked, can exit
+                return
+            }
+        }
+        // If were *here* then there was a problem - trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word) // is the word been used or not
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        // check if the random word can be made out of the letters from another random word
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    // need to use UITextChecker from UIKit. In order to bridge Swift strings to Objective-C strings safely, we need to create an instance of NSRange using the UTF-16 count of our Swift string
+    func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        let allGood = misspelledRange.location == NSNotFound
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    Text("Input word")
+                        .font(.headline)
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                        .onSubmit(addNewWord)
+                        .onAppear(perform: startGame)
+                        .alert(errorTitle, isPresented: $showingError) {
+                            Button("OK") { }
+                        } message: {
+                            Text(errorMessage)
+                        }
+                }
+                Section {
+                    if usedWords.count != 0 {
+                        Text("New words")
+                            .font(.headline)
+                        ForEach(usedWords, id: \.self) { word in
+                            HStack {
+                                Image(systemName: "\(word.count).circle")
+                                Text(word)
+                            }
+                        }
+                    } else {
+                        Text("There are no new words yet")
+                            .foregroundStyle(.secondary)
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Insert") {
+                        addNewWord()
+                    }
+                }
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 
